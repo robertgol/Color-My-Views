22d3724cfb6f502b747ac785af2ba98ff2da6415
Robert <robertgolusinski@gmail.com>
2019-12-28 21:11:19 +0100

    finish the game

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.colormyviews
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val colorSetListener = OnClickListener {
+        it.setBackgroundColor(currentColor ?: Color.GRAY)
+        currentColor = null
+    }
+
+    private var currentColor: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listOf(redButton, greenButton, blueButton).forEach { setCurrentColorChangeListener(it) }
+
+        listOf(
+            boxOneTextView,
+            boxTwoTextView,
+            boxThreeTextView,
+            boxFourTextView,
+            boxFiveTextView
+        ).forEach { it.setOnClickListener(colorSetListener) }
+
+        layout.setOnClickListener {
+            colorSetListener.onClick(it)
+            hideHowToPlayGuide()
+        }
     }
+
+    private fun hideHowToPlayGuide() {
+        howToPlayTextView.visibility = View.GONE
+        guideTextView.visibility = View.GONE
+    }
+
+    private fun setCurrentColorChangeListener(button: Button) {
+        button.setOnClickListener { currentColor = it.getBackgroundColor() }
+    }
+
+    private fun View.getBackgroundColor() = (background as ColorDrawable).color
 }
